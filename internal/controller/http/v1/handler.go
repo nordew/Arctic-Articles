b71e0aad0191d99c46a7618e0c95914fb6398c7b
Roman Horishny <horishny@proton.me>
2024-03-05 20:24:05 +0200

    feat: Handler initialization with route definitions and error handling

---
+++ internal/controller/http/v1/handler.go
@@ -0,0 +1,58 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nordew/ArcticArticles/internal/service"
+	"github.com/nordew/ArcticArticles/pkg/auth"
+	"github.com/nordew/ArcticArticles/pkg/logging"
+	"net/http"
+)
+
+type Handler struct {
+	userService service.UserService
+	auth        auth.Authenticator
+	logger      logging.Logger
+}
+
+func NewHandler(userService service.UserService, auth auth.Authenticator, logger logging.Logger) *Handler {
+	return &Handler{
+		userService: userService,
+		auth:        auth,
+		logger:      logger,
+	}
+}
+
+func (h *Handler) Init() *gin.Engine {
+	app := gin.New()
+
+	auth := app.Group("/auth")
+	{
+		auth.POST("/sign-up", h.signUp)
+		auth.GET("/sign-in", h.signIn)
+		auth.GET("/refresh", h.refresh)
+	}
+
+	return app
+}
+
+func writeErr(c *gin.Context, statusCode int, msg string) {
+	response := gin.H{
+		"status":  "error",
+		"message": msg,
+	}
+
+	c.JSON(statusCode, response)
+}
+
+func validationErr(c *gin.Context) {
+	response := gin.H{
+		"status":  "error",
+		"message": "invalid request body",
+	}
+
+	c.JSON(http.StatusBadRequest, response)
+}
+
+func internalErr(c *gin.Context) {
+	c.JSON(http.StatusInternalServerError, gin.H{})
+}
