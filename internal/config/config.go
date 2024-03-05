877cfbc4bdc35393f8acc7fe90efac2aca899611
Roman Horishny <horishny@proton.me>
2024-03-05 20:23:27 +0200

    feat: Defined configs for postgres, password salt, jwt, http port. Method for getting config from env

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"sync"
+)
+
+type PGConfig struct {
+	PGUser     string `env:"POSTGRES_USER"`
+	PGPassword string `env:"POSTGRES_PASSWORD"`
+	PGPort     string `env:"POSTGRES_CONTAINER_PORT"`
+	PGHost     string `env:"POSTGRES_HOST"`
+	PGDatabase string `env:"POSTGRES_DB"`
+	PGSSLMode  string `env:"POSTGRES_SSL_MODE"`
+}
+
+type Config struct {
+	PGConfig
+
+	Salt     string `env:"SALT"`
+	SignKey  string `env:"SIGN_KEY"`
+	HTTPPort int    `env:"HTTP_PORT"`
+}
+
+var (
+	config *Config
+	once   sync.Once
+)
+
+func GetConfig() (*Config, error) {
+	once.Do(func() {
+		config = &Config{}
+		if err := cleanenv.ReadEnv(config); err != nil {
+			log.Fatalf("failed to parse configs: %v", err)
+		}
+	})
+
+	return config, nil
+}
