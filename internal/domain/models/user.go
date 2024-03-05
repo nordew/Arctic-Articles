d407d521b717a9a731e3b4948d7392ba4ddb0032
Roman Horishny <horishny@proton.me>
2024-03-05 20:25:10 +0200

    feat: Defined User, signIn/Up inputs and validating methods

---
+++ internal/domain/models/user.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"github.com/go-playground/validator/v10"
+	"time"
+)
+
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+}
+
+type User struct {
+	ID           string
+	Email        string
+	Name         string
+	Password     string
+	RefreshToken string
+	Role         int
+	RegisteredAt time.Time
+}
+
+type SignUpInput struct {
+	Name     string `validate:"required"`
+	Email    string `validate:"required,email"`
+	Password string `validate:"required,min=8"`
+}
+
+type SignInInput struct {
+	Email    string `validate:"required,email"`
+	Password string `validate:"required,min=8"`
+}
+
+func (i *SignUpInput) Validate() error {
+	if err := validate.Struct(i); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (i *SignInInput) Validate() error {
+	if err := validate.Struct(i); err != nil {
+		return err
+	}
+
+	return nil
+}
