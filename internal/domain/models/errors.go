e9c74b82d3bc5c85dbd9e6bc7bb953bf0aa96c70
Roman Horishny <horishny@proton.me>
2024-03-07 13:36:37 +0200

    feat: Defined name length error

--- internal/domain/models/errors.go
+++ internal/domain/models/errors.go
@@ -7,4 +7,5 @@ var (
 	ErrEmailAlreadyExists   = errors.New("email already exists")
 	ErrWrongEmailOrPassword = errors.New("wrong email or password")
 	ErrInvalidToken         = errors.New("invalid token")
+	ErrWrongNameLength      = errors.New("name length is more than 32 symbols")
 )
