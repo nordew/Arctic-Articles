1c4b15afdfe1a08f659fef5f2d5f22687104f7b3
Roman Horishny <horishny@proton.me>
2024-03-08 20:51:56 +0200

    feat: Implemented update user method

--- internal/controller/http/v1/profile.go
+++ internal/controller/http/v1/profile.go
@@ -3,6 +3,8 @@ package v1
 import (
 	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/nordew/ArcticArticles/internal/controller/dto"
+	"github.com/nordew/ArcticArticles/internal/domain/models"
 	"net/http"
 )
 
@@ -16,4 +18,54 @@ func (h *Handler) deleteUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{})
+}
+
+func (h *Handler) updateUser(c *gin.Context) {
+	token := getTokenFromCtx(c)
+
+	var updateUserDTO dto.UpdateUserDTO
+	if err := c.ShouldBindJSON(&updateUserDTO); err != nil {
+		validationErr(c)
+		return
+	}
+
+	user, err := h.userService.GetByID(context.Background(), token.Sub)
+	if err != nil {
+		internalErr(c)
+		return
+	}
+
+	if updateUserDTO.Name != "" {
+		if err := models.ValidateUserName(updateUserDTO.Name); err != nil {
+			writeErr(c, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		user.Name = updateUserDTO.Name
+	}
+
+	if updateUserDTO.OldPassword != "" && updateUserDTO.NewPassword != "" {
+		if err := models.ValidateUserPassword(updateUserDTO.NewPassword); err != nil {
+			writeErr(c, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		if err := h.hasher.Compare(user.Password, updateUserDTO.OldPassword); err != nil {
+			writeErr(c, http.StatusBadRequest, "Invalid old password")
+			return
+		}
+
+		hashedPassword, err := h.hasher.Hash(updateUserDTO.NewPassword)
+		if err != nil {
+			writeErr(c, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		user.Password = hashedPassword
+	}
+
+	if err := h.userService.Update(context.Background(), user); err != nil {
+		writeErr(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 }
