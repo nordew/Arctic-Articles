33cfc281dc9cfe35a8f20f539bca22f7f8521a47
Roman Horishny <horishny@proton.me>
2024-03-08 20:53:21 +0200

    feat: JSON tags and validation name method added

--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -13,13 +13,13 @@ func init() {
 }
 
 type User struct {
-	ID           string
-	Email        string
-	Name         string
-	Password     string
-	RefreshToken string
-	Role         int
-	RegisteredAt time.Time
+	ID           string    `json:"id"`
+	Email        string    `json:"email"`
+	Name         string    `json:"name"`
+	Password     string    `json:"password"`
+	RefreshToken string    `json:"refresh_token"`
+	Role         int       `json:"role"`
+	RegisteredAt time.Time `json:"registered_at"`
 }
 
 type SignUpInput struct {
@@ -38,8 +38,12 @@ func (i *SignUpInput) Validate() error {
 		return err
 	}
 
-	if utf8.RuneCountInString(i.Name) > 32 || utf8.RuneCountInString(i.Name) < 2 {
-		return ErrWrongNameLength
+	if err := ValidateUserName(i.Name); err != nil {
+		return err
+	}
+
+	if err := ValidateUserPassword(i.Password); err != nil {
+		return err
 	}
 
 	return nil
@@ -48,6 +52,22 @@ func (i *SignUpInput) Validate() error {
 func (i *SignInInput) Validate() error {
 	if err := validate.Struct(i); err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func ValidateUserName(name string) error {
+	if utf8.RuneCountInString(name) > 32 || utf8.RuneCountInString(name) < 2 {
+		return ErrWrongNameLength
+	}
+
+	return nil
+}
+
+func ValidateUserPassword(password string) error {
+	if utf8.RuneCountInString(password) < 8 {
+		return ErrWrongPasswordLength
 	}
 
 	return nil
