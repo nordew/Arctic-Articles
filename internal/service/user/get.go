a8f1250c8e6fe75bd9bc43156eaa973ab18e322d
Roman Horishny <horishny@proton.me>
2024-03-08 20:50:31 +0200

    refactor: Caching added

--- internal/service/user/get.go
+++ internal/service/user/get.go
@@ -2,9 +2,12 @@ package user
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"github.com/nordew/ArcticArticles/internal/domain/models"
 	"github.com/nordew/ArcticArticles/pkg/auth"
+	"github.com/redis/go-redis/v9"
+	"time"
 )
 
 func (s *userService) SignIn(ctx context.Context, email, password string) (string, string, error) {
@@ -37,5 +40,37 @@ func (s *userService) SignIn(ctx context.Context, email, password string) (strin
 }
 
 func (s *userService) GetByID(ctx context.Context, userID string) (*models.User, error) {
-	return s.userStorage.GetByID(ctx, userID)
+	cachedUser, err := s.redisCl.Get(ctx, userID).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			user, err := s.userStorage.GetByID(ctx, userID)
+			if err != nil {
+				if errors.Is(err, models.ErrUserNotFound) {
+					return nil, err
+				}
+
+				return nil, models.ErrInternal
+			}
+
+			marshalledUser, err := json.Marshal(user)
+			if err != nil {
+				return nil, models.ErrInternal
+			}
+
+			if err := s.redisCl.Set(ctx, userID, marshalledUser, time.Hour*3).Err(); err != nil {
+				return nil, models.ErrInternal
+			}
+
+			return user, nil
+		}
+
+		return nil, models.ErrInternal
+	}
+
+	var user models.User
+	if err := json.Unmarshal([]byte(cachedUser), &user); err != nil {
+		return nil, models.ErrInternal
+	}
+
+	return &user, nil
 }
