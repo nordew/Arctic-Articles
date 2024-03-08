b08bb3eb8a4f9faf7936f0d3378d9a9675f51236
Roman Horishny <horishny@proton.me>
2024-03-08 20:51:43 +0200

    feat: Article & profile endpoints. Get token from context method

--- internal/controller/http/v1/handler.go
+++ internal/controller/http/v1/handler.go
@@ -4,21 +4,30 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/nordew/ArcticArticles/internal/service"
 	"github.com/nordew/ArcticArticles/pkg/auth"
+	"github.com/nordew/ArcticArticles/pkg/hasher"
 	"github.com/nordew/ArcticArticles/pkg/logging"
 	"net/http"
 )
 
 type Handler struct {
-	userService service.UserService
-	auth        auth.Authenticator
-	logger      logging.Logger
+	userService    service.UserService
+	articleService service.ArticleService
+	feedService    service.FeedService
+
+	auth   auth.Authenticator
+	hasher hasher.Hasher
+	logger logging.Logger
 }
 
-func NewHandler(userService service.UserService, auth auth.Authenticator, logger logging.Logger) *Handler {
+func NewHandler(userService service.UserService, articleService service.ArticleService, feedService service.FeedService, auth auth.Authenticator, hasher hasher.Hasher, logger logging.Logger) *Handler {
 	return &Handler{
-		userService: userService,
-		auth:        auth,
-		logger:      logger,
+		userService:    userService,
+		articleService: articleService,
+		feedService:    feedService,
+
+		auth:   auth,
+		hasher: hasher,
+		logger: logger,
 	}
 }
 
@@ -35,10 +44,35 @@ func (h *Handler) Init() *gin.Engine {
 	profile := app.Group("/profile")
 	profile.Use(h.AuthMiddleware)
 	{
-		profile.DELETE("/", h.delete)
+		profile.PATCH("/update", h.updateUser)
+		profile.DELETE("/", h.deleteUser)
+	}
+
+	article := app.Group("/article")
+	{
+		article.GET("/", h.getArticles)
+		article.Use(h.AuthMiddleware)
+		article.POST("/", h.createArticle)
+		article.DELETE("/:id", h.deleteArticle)
 	}
 
 	return app
+}
+
+func getTokenFromCtx(c *gin.Context) *auth.ParseTokenClaimsOutput {
+	claims, exists := c.Get("tokenClaims")
+	if !exists {
+		writeErr(c, http.StatusUnauthorized, "claims not found")
+		return nil
+	}
+
+	tokenClaims, ok := claims.(*auth.ParseTokenClaimsOutput)
+	if !ok {
+		writeErr(c, http.StatusInternalServerError, "auth error")
+		return nil
+	}
+
+	return tokenClaims
 }
 
 func writeErr(c *gin.Context, statusCode int, msg string) {
