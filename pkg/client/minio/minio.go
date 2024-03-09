ce74ee5ab92bf198504cf184572f2146ff4037de
Roman Horishny <horishny@proton.me>
2024-03-09 15:19:34 +0200

    feat: Connectiong to minio

---
+++ pkg/client/minio/minio.go
@@ -0,0 +1,23 @@
+package minioClient
+
+import (
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func NewMinioClient(host, user, password string, ssl bool, port string) (*minio.Client, error) {
+	endpoint := host + ":" + port
+	accessKeyID := user
+	secretAccessKey := password
+	useSSL := ssl
+
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return minioClient, nil
+}
